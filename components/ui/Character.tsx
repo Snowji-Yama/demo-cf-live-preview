6d6cc99284c2a5e0cda7a030dfd59772376efb91
darrouzesc <cdarrouzes@kaliop.com>
2025-05-28 18:58:20 +0200

    add cf props for desc

--- components/ui/Character.tsx
+++ components/ui/Character.tsx
@@ -1,7 +1,7 @@
-import { ContentfulLivePreview } from '@contentful/live-preview';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { ContentfulLivePreview } from "@contentful/live-preview";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-import { Document } from '@contentful/rich-text-types';
+import { Document } from "@contentful/rich-text-types";
 
 export interface CharacterProps {
   id: string;
@@ -17,8 +17,21 @@ const Character = (props: CharacterProps) => {
   return (
     <div className="h-[90%] grid grid-cols-4 gap-20 pt-3 pb-20 px-20">
       <div className="col-start-2 flex flex-col items-center justify-center gap-20">
-        <span {...ContentfulLivePreview.getProps({ entryId: id, fieldId: 'name', locale: 'en-US' })} className="text-8xl font-extrabold">{name}</span>
-        <div>{documentToReactComponents(shortDescription)}</div>
+        <span
+          {...ContentfulLivePreview.getProps({ entryId: id, fieldId: "name", locale: "en-US" })}
+          className="text-8xl font-extrabold"
+        >
+          {name}
+        </span>
+        <div
+          {...ContentfulLivePreview.getProps({
+            entryId: id,
+            fieldId: "shortDescription",
+            locale: "en-US"
+          })}
+        >
+          {documentToReactComponents(shortDescription)}
+        </div>
       </div>
       <img className="col-start-3 w-full h-full object-cover rounded-lg" src={image} alt={name} />
     </div>
