35df22f8e70c01f835ac821f823fec2d3da104fd
darrouzesc <cdarrouzes@kaliop.com>
2025-05-28 18:52:56 +0200

    add cf pros for name

--- components/ui/Character.tsx
+++ components/ui/Character.tsx
@@ -1,3 +1,4 @@
+import { ContentfulLivePreview } from '@contentful/live-preview';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import { Document } from '@contentful/rich-text-types';
@@ -11,12 +12,12 @@ export interface CharacterProps {
 }
 
 const Character = (props: CharacterProps) => {
-  const { name, image, shortDescription } = props;
+  const { id, name, image, shortDescription } = props;
 
   return (
     <div className="h-[90%] grid grid-cols-4 gap-20 pt-3 pb-20 px-20">
       <div className="col-start-2 flex flex-col items-center justify-center gap-20">
-        <span className="text-8xl font-extrabold">{name}</span>
+        <span {...ContentfulLivePreview.getProps({ entryId: id, fieldId: 'name', locale: 'en-US' })} className="text-8xl font-extrabold">{name}</span>
         <div>{documentToReactComponents(shortDescription)}</div>
       </div>
       <img className="col-start-3 w-full h-full object-cover rounded-lg" src={image} alt={name} />
